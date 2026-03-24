049cd946643afa4e7909171fc702bd53ba3519aa
hina714 <aok06242006@gmail.com>
2026-03-24 16:53:44 +0900

    sql/20260323_002_create_cats.sql　削除

--- sql/20260323_002_create_cats.sql
+++
@@ -1,9 +0,0 @@
--- 猫テーブル
-CREATE TABLE cats (
-  id         SERIAL PRIMARY KEY,
-  user_id    UUID NOT NULL,
-  name       TEXT NOT NULL,
-  breed      TEXT,
-  birth_date DATE,
-  created_at TIMESTAMPTZ DEFAULT now()
-);
